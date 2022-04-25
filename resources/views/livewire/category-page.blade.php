bf2ce86ac21973c582ea330bcd2f89021cc1d990
Yusuf Odabaş <yusuf.odabas.78@gmail.com>
2022-04-25 20:43:12 +0300

    Scrool Bug Fix

--- resources/views/livewire/category-page.blade.php
+++ resources/views/livewire/category-page.blade.php
@@ -54,7 +54,7 @@
                             </div>
                         </div>
                     </form>
-                    <div class="widget-media dz-scroll height250 ps ps--active-y">
+                    <div id="DZ_W_Todo{{ $listHeading->id }}" class="widget-media dz-scroll height250 ps ps--active-y">
                         <ul class="timeline">
                             @foreach($listHeading->getListContents as $getContents)
                             <li>
@@ -72,7 +72,7 @@
                             </li>
                             @endforeach
                         </ul>
-                        <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; height: 370px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 322px;"></div></div></div>
+                       </div>
                 </div>
             </div>
         </div>
